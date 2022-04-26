718545f6042fda09121ae149640c22486836108c
pingusl <pingusl@yahoo.fr>
2022-04-26 16:47:24 +0200

    Push pour issue LogoNetflix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
+import logo from "./components/assets/images/logoNetflix.png";
 import arrayOfObjects from "./components/assets/movies_rnexgr.json";
 import Section from "./components/Section";
 
-console.log(arrayOfObjects);
+// console.log(arrayOfObjects);
 function App() {
   return (
     <div className="App">
+      <div className="logo">
+        <img scr={logo} key="logo1" />
+      </div>
       {arrayOfObjects.map((item, index) => {
         return (
           <Section
--- src/components/Section.js
+++ src/components/Section.js
@@ -2,13 +2,11 @@ const Section = (props) => {
   return (
     <div className="section">
       <div className="category">{props.category}</div>
-      {props.arrayOfImages.map((item, index) => {
-        return (
-          <div className="image">
-            <img src={item} id={index} />
-          </div>
-        );
-      })}
+      <div className="carrousel">
+        {props.arrayOfImages.map((item, index) => {
+          return <img className="image" src={item} id={index} />;
+        })}
+      </div>
     </div>
   );
 };
